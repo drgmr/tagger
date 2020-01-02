deb857b41c84b76e33c4e3b8bd83ce2dc8d519d3
Eduardo Cunha <hey@drgmr.com>
2020-01-02 20:45:34 -0300

    Add Repository schema

---
+++ lib/tagger/source_control/repository.ex
@@ -0,0 +1,30 @@
+defmodule Tagger.SourceControl.Repository do
+  @moduledoc """
+  Represents a repository in a external Source Control service.
+  """
+
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @required_fields [:id, :name, :description, :url, :language]
+  @fields @required_fields
+
+  @primary_key {:id, :string, autogenerate: false}
+  @foreign_key_type :string
+  schema "repositories" do
+    field :description, :string
+    field :language, :string
+    field :name, :string
+    field :url, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(repository, attrs) do
+    repository
+    |> cast(attrs, @fields)
+    |> validate_required(@required_fields)
+  end
+end

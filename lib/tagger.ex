089d0fd7ebbff8e4ebb082267dd73fa985ac1582
Eduardo Cunha <hey@drgmr.com>
2020-01-03 01:02:42 -0300

    Add the top level application API module

--- lib/tagger.ex
+++ lib/tagger.ex
@@ -2,4 +2,13 @@ defmodule Tagger do
   @moduledoc """
   Tagger is a small application that tracks tags for GitHub repositories.
   """
+
+  alias Tagger.{Github, SourceControl}
+
+  def get_starred_repositories(username) do
+    with {:ok, repositories} <- Github.get_starred_repositories(username),
+         {:ok, repositories} <- SourceControl.process_repositories(repositories) do
+      {:ok, repositories}
+    end
+  end
 end

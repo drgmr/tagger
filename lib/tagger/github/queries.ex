e4af398e4973faa50de94dea1e3be30281763cc2
Eduardo Cunha <hey@drgmr.com>
2020-01-02 22:45:02 -0300

    Add the Github Queries module

---
+++ lib/tagger/github/queries.ex
@@ -0,0 +1,32 @@
+defmodule Tagger.Github.Queries do
+  @moduledoc """
+  A collection of GraphQL Queries to be run against the Github API.
+  """
+
+  @starred_repositories_query """
+    query($username: String!) {
+      user(login: $username) {
+        starredRepositories(first: 100) {
+          nodes {
+            id
+            name
+            description
+            url
+            languages(first: 1) {
+              nodes {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  """
+
+  def starred_repositories(username) do
+    %{
+      query: @starred_repositories_query,
+      variables: %{username: username}
+    }
+  end
+end
---
+++ test/tagger/github/queries_test.exs
@@ -0,0 +1,16 @@
+defmodule Tagger.Github.QueriesTest do
+  use ExUnit.Case
+
+  alias Tagger.Github.Queries
+
+  @username "drgmr"
+
+  describe "starred_repositories/1" do
+    test "it includes the username on the query data" do
+      %{query: query, variables: variables} = Queries.starred_repositories(@username)
+
+      assert query =~ "$username"
+      assert variables.username == @username
+    end
+  end
+end
